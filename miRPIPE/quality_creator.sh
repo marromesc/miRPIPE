c411cd90f675d7533dd914b1da800a225add29f6
marromesc <mrescorza@gmail.com>
2019-07-01 21:55:19 +0200

    Update quality_creator.sh

--- miRPIPE/quality_creator.sh
+++ miRPIPE/quality_creator.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 ## Copyright 2019 Roman-Escorza M.
 
@@ -19,7 +19,7 @@
 
 if [ $# -eq 0 ]
 then
-  echo "Usage: sh batch_QUALITY.sh QUALITY_parameter_file.txt"
+  echo "Usage: sh quality_creator.sh QUALITY_parameter_file.txt"
   echo "In parameter file, type 0 for mean analysis, 1 for min analysis or 2 for noMM=0"
   exit
 fi
@@ -32,12 +32,7 @@ EXPERIMENT=$( grep -o -m 1 '\<SRP[0-9]*\>' $SRA_TABLE )
 PARAM_FILE="$WORKING_DIR/$EXPERIMENT/logs/parameters_file.txt"
 NUMBER_OF_SAMPLES=$( grep number_of_samples: $PARAM_FILE | awk '{print $2}' )
 NUMBER_OF_ANALYSIS=$( grep number_of_analysis: $QUALITY_PARAM_FILE | awk '{print $2}' )
-ADAPTER_ASK=$( grep question_adap: $PARAM_FILE | awk '{print $2}' )
-
-if [ $ADAPTER_ASK -eq 2 ]
-then
-  ADAPTER=$( grep adapter_sequence: $PARAM_FILE | awk '{print $2}' )
-fi
+ADAPTER=$( grep adapter: $PARAM_FILE | awk '{print $2}' )
 
 i=1
 while [ $i -le $NUMBER_OF_ANALYSIS ]
@@ -45,5 +40,6 @@ do
   TYPE=$( grep mean_or_min_or_noMM_$i: $QUALITY_PARAM_FILE | awk '{print $2}' )
   Q=$( grep q_number_$i: $QUALITY_PARAM_FILE | awk '{print $2}' )
   echo "sh batch_QUALITY.sh $WORKING_DIR $SRA_TABLE $TYPE $Q $ADAPTER" >> $WORKING_DIR/$EXPERIMENT/batch/quality_analysis_commands.bat
+  echo "sh batch_boxplot_quality.sh $QUALITY_PARAM_FILE $TYPE $Q" >> $WORKING_DIR/$EXPERIMENT/batch/quality_boxplot_commands.bat
   i=$(($i+1))
 done
